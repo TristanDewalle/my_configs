a8df59e886032b6293983409a28fbdac86a581e4
Tristan Dewalle <tristan.dewalle@gmail.com>
2019-08-17 16:12:40 +0200

    add i3 config file and Xresources for rxvt-unicode plus two distinct files for windows and linux

---
+++ linux_link.sh
@@ -0,0 +1,14 @@
+#vim
+ln -s ./init.vim ~/.vimrc
+#neovim
+ln -s ./init.vim ~/.config/nvim/init.vim
+
+#flake8
+#for linux
+ln -s ./flake8 ~/.config/flake8
+
+#i3
+ln -s ./i3config ~/.config/i3/config
+
+#urxvt
+ln -s ./Xresources ~/.Xresources
--- win_link.sh
+++ win_link.sh
@@ -1,8 +1,7 @@
+#Links for windows
 #vim
 ln -s ~/dotfiles/init.vim ~/.vimrc
 
 #flake8
 #for windows
 ln -s ~/dotfiles/flake8 ~/.flake8
-#for linux
-#ln -s ./flake8 ~/.config/flake8
